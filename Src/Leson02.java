e6c2f439726b16c5b823a7b41183b4ff093fc4c6
NatalyaKUZYK <2598.nata.kuzyk@gmail.com>
2023-08-08 17:22:34 +0300

    third message

--- Src/Leson02.java
+++ Src/Leson02.java
@@ -2,5 +2,6 @@ public class Leson02 {
     public static void main(String[]args){
         System.out.println("Hello git");
         System.out.println("Hello git from Natalya");
+        System.out.println(" Hello git for our group");
     }
 }
